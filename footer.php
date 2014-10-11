0e23d8f423ac4918a61ec2916a573afeec5e2bc8
Spencer Piontkowski <spiontkowski@psrm.org>
2014-10-11 15:23:21 -0700

    Add site footer.

--- footer.php
+++ footer.php
@@ -12,7 +12,23 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-
+            <div class="footer-buffer clearfix">&nbsp;</div>
+            <div class="row">
+                <div class="col-sm-4 col-sm-offset-2">
+                    <span class="heading">Main Facility & Train Rides</span><br/>
+                    <span><a href="#">Campo Depot</a></span><br/>
+                    <span class="address-line-1">750 Depot Street</span><br/>
+                    <span class="address-line-2">Campo, California 91906</span><br/>
+                    <span class="telephone"><a href="tel:6194789937">(619) 478-9937</a></span>
+                </div>
+                <div class="col-sm-5 col-sm-offset-1">
+                    <span class="heading">Business Office</span><br/>
+                    <span><a href="#">La Mesa Depot</a></span><br/>
+                    <span class="address-line-1">4695 Nebo Drive</span><br/>
+                    <span class="address-line-2">La Mesa, California 91941</span><br/>
+                    <span class="telephone"><a href="tel:6194657776">(619) 465-PSRM (465-7776) <i>recorded information</i></a></span>
+                </div>
+            </div>
         </div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
