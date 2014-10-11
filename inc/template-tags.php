76e2b1fe20db025c2490ffb508f5738df03d6d6d
Spencer Piontkowski <spiontkowski@psrm.org>
2014-10-11 14:55:48 -0700

    Update Posted On function and remove byline.

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -67,9 +67,6 @@ if ( ! function_exists( 'psrm_posted_on' ) ) :
  */
 function psrm_posted_on() {
 	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
-	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
-	}
 
 	$time_string = sprintf( $time_string,
 		esc_attr( get_the_date( 'c' ) ),
@@ -83,12 +80,7 @@ function psrm_posted_on() {
 		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
 	);
 
-	$byline = sprintf(
-		_x( 'by %s', 'post author', 'psrm' ),
-		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
-	);
-
-	echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>';
+	echo '<span class="posted-on">' . $posted_on . '</span>';
 
 }
 endif;
