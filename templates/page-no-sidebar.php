0fd1e52ced758799f6edddedc1129634d3455a07
Spencer Piontkowski <spiontkowski@psrm.org>
2014-10-19 10:29:46 -0700

    Add page template with no sidebar.

---
+++ templates/page-no-sidebar.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Template Name: No Sidebar
+ * Description: A Page Template for displaying no sidebar.
+ */
+
+get_header(); ?>
+
+<div class="row">
+
+    <div id="primary" class="content-area col-sm-12">
+        <main id="main" class="site-main" role="main">
+
+            <?php while ( have_posts() ) : the_post(); ?>
+
+                <?php get_template_part( 'content', 'page' ); ?>
+
+            <?php endwhile; // end of the loop. ?>
+
+        </main><!-- #main -->
+    </div><!-- #primary -->
+</div>
+<?php get_footer(); ?>
