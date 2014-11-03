26b31fb9e0c7c114ab4b6c96623d399b71935451
Spencer Piontkowski <spiontkowski@psrm.org>
2014-11-03 06:40:50 -0800

    Add depot links to footer.

--- footer.php
+++ footer.php
@@ -16,14 +16,14 @@
             <div class="row">
                 <div class="col-sm-4 col-sm-offset-2">
                     <span class="heading">Main Facility & Train Rides</span><br/>
-                    <span><a href="#">Campo Depot</a></span><br/>
+                    <span><a href="visitor-information/find-us/">Campo Depot</a></span><br/>
                     <span class="address-line-1">750 Depot Street</span><br/>
                     <span class="address-line-2">Campo, California 91906</span><br/>
                     <span class="telephone"><a href="tel:6194789937">(619) 478-9937</a></span>
                 </div>
                 <div class="col-sm-5 col-sm-offset-1">
                     <span class="heading">Business Office</span><br/>
-                    <span><a href="#">La Mesa Depot</a></span><br/>
+                    <span><a href="visitor-information/la-mesa-depot/">La Mesa Depot</a></span><br/>
                     <span class="address-line-1">4695 Nebo Drive</span><br/>
                     <span class="address-line-2">La Mesa, California 91941</span><br/>
                     <span class="telephone"><a href="tel:6194657776">(619) 465-PSRM (465-7776) <i>recorded information</i></a></span>
