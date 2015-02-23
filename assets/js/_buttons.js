a90ce92c08c16c81e1c8ecdecb59d354e0e6d6dc
Spencer Piontkowski <spiontkowski@psrm.org>
2015-02-23 09:01:27 -0800

    Remove buttons.js to the PSRM plugin.

--- assets/js/_buttons.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function($){
-    $('form').submit(function(){
-        buttons = $('input[type="submit"], button[type="submit"]');
-        buttons.prop('disabled', true);
-        buttons.val('Submitting...');
-        return true;
-    });
-});
